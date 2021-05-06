1169baacd57bf153a049cf014be44dacae943969
Nguyễn Bá Đức <62656512+lukamix@users.noreply.github.com>
2021-05-06 21:25:06 +0700

    Add config , build model

---
+++ src/java/Model/User_PlaylistModel.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class User_PlaylistModel {
+   private int playlistid;
+   private int userid;
+
+    public int getPlaylistid() {
+        return playlistid;
+    }
+
+    public void setPlaylistid(int playlistid) {
+        this.playlistid = playlistid;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+   
+}
