44d683df2f5ef38d419dc2bb96941e083317faae
Nguyễn Khương Duy <74958626+DuyNK77@users.noreply.github.com>
2021-05-16 16:26:41 +0700

    Add All DAO
    Add some needed DAO with query

---
+++ src/java/DAO/IUser_PlaylistDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.User_PlaylistModel;
+
+/**
+ *
+ * @author PV
+ */
+public interface IUser_PlaylistDAO extends GenericDAO<User_PlaylistModel> {
+    
+}
